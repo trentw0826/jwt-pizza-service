09f7801bf1a318e194556fdad11266d02542c5f1
trentw0826 <trentw0826@gmail.com>
2026-01-30 12:39:53 -0700

    Add initial basic service tests for auth router

--- test/authrouter.test.js
+++ test/authrouter.test.js
@@ -1,3 +1,30 @@
-test("1 + 1 equals 2", () => {
-  expect(1 + 1).toBe(2);
+const request = require("supertest");
+const app = require("../src/service");
+
+const testUser = { name: "pizza diner", email: "", password: "a" };
+let testUserAuthToken;
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
+  );
+}
+
+describe("Auth Router", function () {
+  beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+    const registerRes = await request(app).post("/api/auth").send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    expectValidJwt(testUserAuthToken);
+  });
+
+  test("should authenticate user with valid credentials", async () => {
+    const loginRes = await request(app).put("/api/auth").send(testUser);
+    expect(loginRes.status).toBe(200);
+    expectValidJwt(loginRes.body.token);
+
+    const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+    delete expectedUser.password;
+    expect(loginRes.body.user).toMatchObject(expectedUser);
+  });
 });
